56e8c819e440a486e70eab3bf1808cc3328489dc
Tejas Jadhav <tjadhav95@gmail.com>
2021-09-19 14:55:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRjdppDnrZMNjBwoSLkI-nzNvtu0qWb3I",
-  authDomain: "fireact-chatter.firebaseapp.com",
-  projectId: "fireact-chatter",
-  storageBucket: "fireact-chatter.appspot.com",
-  messagingSenderId: "262918477135",
-  appId: "1:262918477135:web:f20e497b95c2184cd0a13a"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXX.firebaseapp.com",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXr",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXX.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
